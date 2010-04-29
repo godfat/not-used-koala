d2a514837c87bde2a2fe721170f278b63d124e45
Lin Jen-Shin <godfat@godfat.org>
2010-04-29 23:55:26 +0800

    show you your access_token parsed from cookies

--- example/example.rb
+++ example/example.rb
@@ -5,8 +5,15 @@ class Example
   def self.read path
     File.read(File.join(File.dirname(__FILE__), path))
   end
+
+  def self.parse_cookie env
+    Rack::Utils.parse_query(env['HTTP_COOKIE'].match(/"(.+?)"/)[1])
+  end
+
   def call env
-    app_id = self.class.read('app_id.txt').strip
+    app_id       = self.class.read('app_id.txt').strip
+    access_token = self.class.parse_cookie(env)['access_token']
+
     [200, {}, [ERB.new(self.class.read('example.html')).result(binding)]]
   end
 end
