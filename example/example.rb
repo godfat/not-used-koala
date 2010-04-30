de47ff8907859b58d8812ba251b6c860d1a81628
Lin Jen-Shin <godfat@godfat.org>
2010-04-30 15:12:04 +0800

    example.rb: like current url

--- example/example.rb
+++ example/example.rb
@@ -16,13 +16,18 @@ class Example
     YAML.load(read('example.yaml'))[key]
   end
 
+  def self.current_url e
+    "#{e[rack.url_scheme]}://#{e['HTTP_HOST']}#{e['REQUEST_PATH']}" \
+                            "#{e['QUERY_STRING']}"
+  end
+
   def call env
     app_id       = self.class.read('app_id.txt').strip
     access_token = self.class.parse_cookie(env)['access_token']
 
     title        = self.class.read_config('title')
     site_name    = self.class.read_config('site_name')
-    link         = env['REQUEST_URI']
+    link         = self.class.current_url(env)
 
     [200, {}, [ERB.new(self.class.read('example.html')).result(binding)]]
   end
