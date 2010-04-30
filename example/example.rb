bb9b380ab648d0a1cd6e5843d84989730b2ae557
Lin Jen-Shin <godfat@godfat.org>
2010-04-30 15:06:03 +0800

    extract config

--- example/example.rb
+++ example/example.rb
@@ -1,5 +1,6 @@
 
 require 'erb'
+require 'yaml'
 
 class Example
   def self.read path
@@ -11,9 +12,17 @@ class Example
     Rack::Utils.parse_query($1)
   end
 
+  def self.read_config key
+    YAML.load(read('example.yaml'))[key]
+  end
+
   def call env
     app_id       = self.class.read('app_id.txt').strip
     access_token = self.class.parse_cookie(env)['access_token']
+
+    title        = self.class.read_config('title')
+    site_name    = self.class.read_config('site_name')
+    link         = env['REQUEST_URI']
 
     [200, {}, [ERB.new(self.class.read('example.html')).result(binding)]]
   end
